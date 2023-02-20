3384efa2b35638a440d2e87e950bd19d06f0d5e5
JLoGukA <JLoGuka@gmail.com>
2023-02-20 14:27:48 +0300

    Commented code that is not needed anymore. Gonna delete it later.

--- musicVoteSketch.ino
+++ musicVoteSketch.ino
@@ -15,11 +15,11 @@ const char* password = "KahaEichi@";
 const char* host = "bmstuvoting.herokuapp.com";
 const int httpsPort = 443;
 
-String winner;
+// String winner;
 //DynamicJsonDocument times(256); // json, который хранит массив дат, в которые проигрывается музыка и звонок
-DynamicJsonDocument music(4096); // json, который хранит структуру голосами, для выявляния победителя
+// DynamicJsonDocument music(4096); // json, который хранит структуру голосами, для выявляния победителя
 SoftwareSerial MS(13, 15); // uart для dfplayer mini
-String beginArr[] = { "08:40:00", "10:25:00", "12:50:00", "14:35:00" };// начало пар
+// String beginArr[] = { "08:40:00", "10:25:00", "12:50:00", "14:35:00" };// начало пар
 
 GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
 iarduino_RTC watch(RTC_DS3231);
@@ -28,154 +28,154 @@ DFRobotDFPlayerMini player;
 
 //MySQL_Connection conn((Client *)&wificlient);
 
-IPAddress server_addr(192,168,3,3);
-char dbuser[]="root";
-char dbpassword[]="0000";
+IPAddress server_addr(62,122,196,47);
+// char dbuser[]="root";
+// char dbpassword[]="0000";
 
-String hostsite = "http://192.168.3.3:3005/get/schedule";
+// String hostsite = "http://62.122.196.47:40005/get/schedule";
 
 
 //================================================================================================
 //                                  Установка времени
 //================================================================================================
-void setDateTime()
-{
-    const char* strM = "JanFebMarAprMayJunJulAugSepOctNovDec";    // Определяем массив всех вариантов текстового представления текущего месяца находящегося в предопределенном макросе __DATE__.
-    const char* sysT = __TIME__;                                  // Получаем время компиляции скетча в формате "SS:MM:HH".
-    const char* sysD = __DATE__;                                  // Получаем дату  компиляции скетча в формате "MMM:DD:YYYY", где МММ - текстовое представление текущего месяца, например: Jul.
-    //  Парсим полученные значения в массив:                    // Определяем массив «i» из 6 элементов типа int, содержащий следующие значения: секунды, минуты, часы, день, месяц и год компиляции скетча.
-    const int i[6]{ (sysT[6] - 48) * 10 + (sysT[7] - 48),
-    (sysT[3] - 48) * 10 + (sysT[4] - 48),
-    (sysT[0] - 48) * 10 + (sysT[1] - 48),
-    (sysD[4] - 48) * 10 + (sysD[5] - 48),
-    ((int)memmem(strM,36,sysD,3) + 3 - (int)&strM[0]) / 3,
-    (sysD[9] - 48) * 10 + (sysD[10] - 48) };
-    watch.settime(i[0] + 40, i[1], i[2], i[3], i[4], i[5]);              // Устанавливаем время
-}
+// void setDateTime()
+// {
+//     const char* strM = "JanFebMarAprMayJunJulAugSepOctNovDec";    // Определяем массив всех вариантов текстового представления текущего месяца находящегося в предопределенном макросе __DATE__.
+//     const char* sysT = __TIME__;                                  // Получаем время компиляции скетча в формате "SS:MM:HH".
+//     const char* sysD = __DATE__;                                  // Получаем дату  компиляции скетча в формате "MMM:DD:YYYY", где МММ - текстовое представление текущего месяца, например: Jul.
+//     //  Парсим полученные значения в массив:                    // Определяем массив «i» из 6 элементов типа int, содержащий следующие значения: секунды, минуты, часы, день, месяц и год компиляции скетча.
+//     const int i[6]{ (sysT[6] - 48) * 10 + (sysT[7] - 48),
+//     (sysT[3] - 48) * 10 + (sysT[4] - 48),
+//     (sysT[0] - 48) * 10 + (sysT[1] - 48),
+//     (sysD[4] - 48) * 10 + (sysD[5] - 48),
+//     ((int)memmem(strM,36,sysD,3) + 3 - (int)&strM[0]) / 3,
+//     (sysD[9] - 48) * 10 + (sysD[10] - 48) };
+//     watch.settime(i[0] + 40, i[1], i[2], i[3], i[4], i[5]);              // Устанавливаем время
+// }
 //================================================================================================
 //                                  Запросы к серверу
 //================================================================================================
 
-void getMusic(String tm)
-{
-    WiFiClientSecure wificlient;
+// void getMusic(String tm)
+// {
+//     WiFiClientSecure wificlient;
     
-    wificlient.setInsecure();//пропустить верификацию (из-за https)
-    if (wificlient.connect(host, httpsPort))
-    {
-        Serial.println("Get music at " + tm);
-        // Make a HTTP request:
-        wificlient.println("GET https://" + String(host) + "/api_form/music_list/" + tm + " HTTP/1.1");
-        wificlient.println("Host: " + String(host));
-        wificlient.println("Connection: close");
-        wificlient.println();
+//     wificlient.setInsecure();//пропустить верификацию (из-за https)
+//     if (wificlient.connect(host, httpsPort))
+//     {
+//         Serial.println("Get music at " + tm);
+//         // Make a HTTP request:
+//         wificlient.println("GET https://" + String(host) + "/api_form/music_list/" + tm + " HTTP/1.1");
+//         wificlient.println("Host: " + String(host));
+//         wificlient.println("Connection: close");
+//         wificlient.println();
 
-        while (wificlient.connected())
-            if (wificlient.readStringUntil('\n') == "\r") break; // пропускаем хедер
+//         while (wificlient.connected())
+//             if (wificlient.readStringUntil('\n') == "\r") break; // пропускаем хедер
 
-        deserializeJson(music, wificlient); // сохраняем json
-    }
-    else Serial.println("Connection failed!");
-    wificlient.stop();
-}
+//         deserializeJson(music, wificlient); // сохраняем json
+//     }
+//     else Serial.println("Connection failed!");
+//     wificlient.stop();
+// }
 
-void getTime()
-{
-    WiFiClientSecure client;
-    client.setInsecure();//пропустить верификацию(из-за https)
-    if (client.connect(host, httpsPort))
-    {
-        Serial.println("Get time array");
-        // Make a HTTP request:
-        client.println("GET https://" + String(host) + "/api_form/time HTTP/1.1");
-        client.println("Host: " + String(host));
-        client.println("Connection: close");
-        client.println();
+// void getTime()
+// {
+//     WiFiClientSecure client;
+//     client.setInsecure();//пропустить верификацию(из-за https)
+//     if (client.connect(host, httpsPort))
+//     {
+//         Serial.println("Get time array");
+//         // Make a HTTP request:
+//         client.println("GET https://" + String(host) + "/api_form/time HTTP/1.1");
+//         client.println("Host: " + String(host));
+//         client.println("Connection: close");
+//         client.println();
 
-        while (client.connected())
-            if (client.readStringUntil('\n') == "\r") break; // пропускаем хедер
+//         while (client.connected())
+//             if (client.readStringUntil('\n') == "\r") break; // пропускаем хедер
 
-        //deserializeJson(times, client); // сохраняем json
-    }
-    else Serial.println("Connection failed!");
-    client.stop();
-}
+//         //deserializeJson(times, client); // сохраняем json
+//     }
+//     else Serial.println("Connection failed!");
+//     client.stop();
+// }
 
-void resetVotes(String tm) // метод обнуляет голоса в бд в определённый перерыв
-{
-    WiFiClientSecure client;
-    client.setInsecure();//пропустить верификацию (из-за https)
-    if (client.connect(host, httpsPort))
-    {
-        Serial.println("reset votes at " + tm);
-        // Make a HTTP request:
-        client.println("DELETE https://" + String(host) + "/api_form/erase/" + tm + " HTTP/1.1");
-        client.println("Host: " + String(host));
-        client.println("Connection: close");
-        client.println();
-    }
-    else Serial.println("Connection failed!");
-    client.stop();
-}
+// void resetVotes(String tm) // метод обнуляет голоса в бд в определённый перерыв
+// {
+//     WiFiClientSecure client;
+//     client.setInsecure();//пропустить верификацию (из-за https)
+//     if (client.connect(host, httpsPort))
+//     {
+//         Serial.println("reset votes at " + tm);
+//         // Make a HTTP request:
+//         client.println("DELETE https://" + String(host) + "/api_form/erase/" + tm + " HTTP/1.1");
+//         client.println("Host: " + String(host));
+//         client.println("Connection: close");
+//         client.println();
+//     }
+//     else Serial.println("Connection failed!");
+//     client.stop();
+// }
 //================================================================================================
 //                                      Определение победителя
 //================================================================================================
 
-int Winner() // метод определяет победителя голосования
-{
-    int index = 0;
-    int vote = 0;
-    JsonArray Music = music["music"];
-    for (int i = 0; i < Music.size(); i++)
-    {
-        if (Music[i]["vote"].as<int>() >= vote)
-        {
-            vote = Music[i]["vote"].as<int>();
-            winner = Music[i]["name"].as<String>();
-            index = i;
-        }
-    }
-    return index + 1;
-}
+// int Winner() // метод определяет победителя голосования
+// {
+//     int index = 0;
+//     int vote = 0;
+//     JsonArray Music = music["music"];
+//     for (int i = 0; i < Music.size(); i++)
+//     {
+//         if (Music[i]["vote"].as<int>() >= vote)
+//         {
+//             vote = Music[i]["vote"].as<int>();
+//             winner = Music[i]["name"].as<String>();
+//             index = i;
+//         }
+//     }
+//     return index + 1;
+// }
 
 //================================================================================================
 //                                        Работа с экраном
 //================================================================================================
 
-void drawLines()                 // рисует разметку
-{
-    oled.line(0, 20, 128, 20);
-    oled.line(0, 41, 128, 41);
-}
-void printDate(String D)        // отображает дату
-{
-    oled.setScale(2);
-    oled.setCursor(5, 0);
-    oled.print(D);
-}
-void printTime(String T)       // отображает время
-{
-    oled.setScale(2);
-    oled.setCursor(15, 3);
-    oled.print(T);
-}
-void printLoading()            // отображает надпись loading
-{
-    oled.setScale(2);
-    oled.setCursor(20, 3);
-    oled.print("loading");
-}
-void printWinner()             // отображает победителя голосования
-{
-    int index = winner.indexOf(" - ");
-    String au = winner.substring(0, index), comp = winner.substring(index + 3, winner.length());
-    //строка парсится на автора и композицию и центрируется
-    oled.setScale(1);
-    oled.setCursor((128 - 6 * au.length()) / 2, 6);
-    oled.print(au);
-    oled.setCursor((128 - 6 * comp.length()) / 2, 7);
-    oled.print(comp);
-}
+// void drawLines()                 // рисует разметку
+// {
+//     oled.line(0, 20, 128, 20);
+//     oled.line(0, 41, 128, 41);
+// }
+// void printDate(String D)        // отображает дату
+// {
+//     oled.setScale(2);
+//     oled.setCursor(5, 0);
+//     oled.print(D);
+// }
+// void printTime(String T)       // отображает время
+// {
+//     oled.setScale(2);
+//     oled.setCursor(15, 3);
+//     oled.print(T);
+// }
+// void printLoading()            // отображает надпись loading
+// {
+//     oled.setScale(2);
+//     oled.setCursor(20, 3);
+//     oled.print("loading");
+// }
+// void printWinner()             // отображает победителя голосования
+// {
+//     int index = winner.indexOf(" - ");
+//     String au = winner.substring(0, index), comp = winner.substring(index + 3, winner.length());
+//     //строка парсится на автора и композицию и центрируется
+//     oled.setScale(1);
+//     oled.setCursor((128 - 6 * au.length()) / 2, 6);
+//     oled.print(au);
+//     oled.setCursor((128 - 6 * comp.length()) / 2, 7);
+//     oled.print(comp);
+// }
 //================================================================================================
 //                                      Инициализация прибора
 //================================================================================================
@@ -226,9 +226,16 @@ void setup(){
     MS.begin(9600);
     Serial.begin(115200);
     oled.init();
-    oled.clear();                     
-    drawLines();
-    printLoading();
+    oled.clear(); 
+
+    //draw lines
+    oled.line(0, 20, 128, 20);
+    oled.line(0, 41, 128, 41);
+
+    //print "loading"
+    oled.setScale(2);
+    oled.setCursor(20, 3);
+    oled.print("loading");
 
     WiFi.mode(WIFI_OFF);    //Prevents reconnection issue (taking too long to connect)
     WiFi.mode(WIFI_STA);    //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
@@ -243,8 +250,8 @@ void setup(){
     timeClient.update();
     
 
-    wificlient.connect(server_addr,3005);
-    http.begin(wificlient,"http://192.168.40.192:3005/get/schedule");
+    wificlient.connect(server_addr,40005);
+    http.begin(wificlient,"http://62.122.196.47:40005/get/schedule");
     http.GET();
     String bytes = http.getString();
     http.end();
@@ -257,9 +264,8 @@ void setup(){
     }
     setNextTime(timeArrSize);
     
-
     if(playerready = player.begin(MS)){
-      player.volume(5);
+      player.volume(3);
       player.EQ(DFPLAYER_EQ_NORMAL);
       player.outputDevice(DFPLAYER_DEVICE_SD);
     }
@@ -305,8 +311,8 @@ void loop(){
     //if(timeClient.getHours()==testtime.tm_hour&&timeClient.getMinutes()>=testtime.tm_min){
     if(timeClient.getHours()==times[nexttime].tm_hour&&timeClient.getMinutes()>=times[nexttime].tm_min){
       nexttime = (nexttime+1)%timeArrSize;
-      wificlient.connect(server_addr,3005);
-      http.begin(wificlient,"http://192.168.40.192:3005/get/schedule");
+      wificlient.connect(server_addr,40005);
+      http.begin(wificlient,"http://62.122.196.47:40005/get/winner");
       http.GET();
       String bytes = http.getString();
       http.end();
@@ -314,7 +320,7 @@ void loop(){
       int win = atoi(doc["win"]);
 
       if (playerready){
-        player.volume(5); 
+        player.volume(3); 
         player.playMp3Folder(win);
         
       } 
